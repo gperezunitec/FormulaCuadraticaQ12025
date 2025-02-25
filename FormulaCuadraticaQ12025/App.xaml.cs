eba62191fb06a37e4a4ffa1030ecae3067d72861
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-02-24 18:28:28 -0600

    Add project files.

---
+++ FormulaCuadraticaQ12025/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FormulaCuadraticaQ12025
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

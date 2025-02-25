d2123b4c4f290ed5cbf6f9ea968fbbfb0a2c9885
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-02-24 20:24:39 -0600

    viewmodel namespace

--- FormulaCuadraticaQ12025/Views/FOrmulaCuadraticaView.xaml.cs
+++ FormulaCuadraticaQ12025/Views/FOrmulaCuadraticaView.xaml.cs
@@ -2,6 +2,8 @@ namespace FormulaCuadraticaQ12025.Views;
 
 public partial class FOrmulaCuadraticaView : ContentPage
 {
+
+	FOrmulaCuadraticaView viewModel;
 	public FOrmulaCuadraticaView()
 	{
 		InitializeComponent();

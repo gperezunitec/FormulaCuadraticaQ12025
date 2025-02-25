f304264408d6ba777db858810a626f072ae8c7d8
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-02-24 18:35:42 -0600

    Main page deleted

---
+++ FormulaCuadraticaQ12025/ViewModels/FormulaCuadraticaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaCuadraticaQ12025.ViewModels
+{
+    internal class FormulaCuadraticaViewModel
+    {
+    }
+}

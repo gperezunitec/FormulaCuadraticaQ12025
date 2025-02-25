e4d4968d145b7c77297f76caa08f0f5620791f4a
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-02-24 19:19:10 -0600

    viewModel

--- FormulaCuadraticaQ12025/App.xaml.cs
+++ FormulaCuadraticaQ12025/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace FormulaCuadraticaQ12025
+﻿using FormulaCuadraticaQ12025.Views;
+
+namespace FormulaCuadraticaQ12025
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new FOrmulaCuadraticaView();
         }
     }
 }
